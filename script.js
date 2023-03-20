fa4ef4a2bbade8411f2883c7e59ef0e32bfb2d4a
aarontalua2 <aarontalua14@gmail.com>
2023-03-20 19:27:18 +0800

    Finished the project

--- script.js
+++ script.js
@@ -1,33 +1,91 @@
 function getComputerChoice() {
-  let choices = ["rock", "paper", "scissor"];
+  // Randomly select a choice for computer
+  // -- floor/random generates numbers 0-2 and uses them as index to get value from "choices" array.
   let computerSelection = Math.floor(Math.random() * choices.length);
   computerSelection = choices[computerSelection];
-  console.log(computerSelection);
   return computerSelection;
 }
 
-function roundLost() {
-  console.log("You lost.");
-}
-
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection.toLowerCase() == "rock") {
-    computerSelection == "paper"
-      ? roundLost()
-      : computerSelection == "scissor"
-      ? roundWon()
-      : computerSelection == "rock"
-      ? roundDraw()
-      : console.log("Invalid round.");
+  // Result strings to reduce redundancy, as these will be used multiple times.
+  let won = `You won, ${playerSelection} beats ${computerSelection}.`;
+  let lost = `You lost, ${computerSelection} beats ${playerSelection}.`;
+  let draw = `It's a draw. You are both ${playerSelection}.`;
+
+  // Checks player selection and then
+  // Checks computer selection and prints results, then add depending on the result.
+  switch (playerSelection) {
+    case "rock":
+      switch (computerSelection) {
+        case "rock":
+          console.log(draw);
+          break;
+        case "paper":
+          console.log(lost);
+          computerScore++;
+          break;
+        case "scissor":
+          console.log(won);
+          playerScore++;
+          break;
+      }
+      break;
+
+    case "paper":
+      switch (computerSelection) {
+        case "paper":
+          console.log(draw);
+          break;
+        case "scissor":
+          console.log(won);
+          playerScore++;
+          break;
+        case "rock":
+          console.log(lost);
+          computerScore++;
+      }
+      break;
+
+    case "scissor":
+      switch (computerSelection) {
+        case "scissor":
+          console.log(draw);
+          break;
+        case "rock":
+          console.log(lost);
+          computerScore++;
+          break;
+        case "paper":
+          console.log(won);
+          playerScore++;
+          break;
+      }
+      break;
   }
 }
 
 function game() {
-  for (i = 1; i < 5; i++) {
-    playRound();
+  // Introduction
+  console.log(
+    `Welcome to my rock-paper-scissor javascript game. Please choose between ${choices} to start the round.\n`
+  );
+
+  // Loop to play 5 rounds.
+  for (i = 1; i <= 5; i++) {
+    playerSelection = prompt(`Please choose "rock", "paper", or "scissor"`);
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
   }
+
+  console.log(
+    `Thank you for playing the game. The final final score is: \n
+    You: ${playerScore} vs Computer: ${computerScore}.`
+  );
 }
 
-playerSelection = "rock";
+// Global variables and function calls -- basically using the program.
+const choices = ["rock", "paper", "scissor"];
+let playerScore = 0;
+let computerScore = 0;
 
-playRound(playerSelection, computerSelection);
+game();
